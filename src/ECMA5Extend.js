687630d31984abb902379f26983a7d3d0ac17cf9
Anzor Bashkhaz <anzorb@gmail.com>
2014-02-19 14:41:14 -0500

    added strict mode

--- src/ECMA5Extend.js
+++ src/ECMA5Extend.js
@@ -14,7 +14,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 ( function(root, factory) {
+	
+		"use strict";
+
 		if ( typeof define === 'function' && define.amd) {
 			define(factory);
 		} else {
@@ -29,7 +33,7 @@
 				}
 			};
 
-			if (typeof module !== "undefined") {
+			if ( typeof module !== "undefined") {
 				module.exports = {
 					createType : ECMA5Extend.createType
 				};
